8bb0ccbd9e0bb401e6464462d0bf9ed5ecbf842f
P373RW4NG <peter.w4ng@gmail.com>
2017-07-05 12:05:02 +0800

    Update and rename src/server-RPi3/filter.h to src/server-RPi3/algorithm/filter.h

--- src/server-RPi3/algorithm/filter.h
+++ src/server-RPi3/algorithm/filter.h
@@ -1,6 +1,7 @@
 #ifndef FILTER_H
 #define FILTER_H
 
+//complementary filter
 float complFtr(float output, float ang_a, float g, float dt, float a){
     output = a*(output + g*dt) + (1-a)*ang_a;
     return output;
